75ff4a28d42fcd68856177cd4bee83e5b8c77c4e
William Halliday <58829019+vaeproj@users.noreply.github.com>
2022-06-22 02:17:54 +0100

    Update week15.py
    - Removed for loop as can be deprecated for print() in this instance. 
    - Removed "unique_name" declaration as assignment is not needed.
    - Removed ranged for loop as not needed.
    - Replaced concatenation with + operand for f-string to conform to pep8 and increased speed.
    - Replaced "string_generator" function with list comprehension, for more concise and fast code.

--- week15.py
+++ week15.py
@@ -4,29 +4,13 @@
 
 """
 import random
-import string
-def string_generator(size=7, string=string.ascii_letters + string.digits):
-    return ''.join(random.choice(string) for _ in range(size))
-    
-#Allow the user to input the name of their department that is used in the unique name
+
+# Allow the user to input the name of their department that is used in the unique name
+
 number = int(input("enter number of unique names of EC2 instances: "))
 department = input("Enter one of these department: Human Resources, Accounting, FinOps: ")
-for _ in department:
-    if department == "Human Resources":
-        print("Human Resources")
-        break
-    elif department == "Accounting":
-        print("Accounting")
-        break
-    elif department == "FinOps":
-        print("FinOps")
-        break
 
-#Print out unique names of instances
+# Print out unique names of instances
 
-for _ in range(1, number +1):
-    Unique_name = department
-    ec2_identity = unique_name + "-" +string_generator()
-    print("your EC2's unique name is:", ec2_identity)
-    
-    
+ec2_identity = f"{department}-{''.join([chr(random.randint(97,123)) for i in range(7)])}"
+print("your EC2's unique name is:", ec2_identity)
